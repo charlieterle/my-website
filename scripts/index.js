187fc7b2b6ce8f37c56108ca1477078e453297a0
charlieterle <cdieterle33@gmail.com>
2024-07-10 21:09:59 +0900

    Change font to noto-sans-jp and make topnav color responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,24 @@
-function openMenu() {
-    var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+function toggleMenu() {
+    let topnav = document.getElementById("topnav");
+    if (topnav.classList.contains("responsive")) {
+        topnav.classList.remove("responsive");
+    }
+    else {
+        topnav.classList.add("responsive");
     }
 }
+
+function menuActivate(e) {
+    const next = e.target;
+    const previous = document.getElementsByClassName("active")[0];
+    previous.className = "";
+    next.className = "active";
+}
+
+const aboutLink = document.getElementById("about-link");
+const contactLink = document.getElementById("contact-link");
+const projectsLink = document.getElementById("projects-link");
+
+aboutLink.addEventListener('click', (event) => menuActivate(event));
+contactLink.addEventListener('click', (event) => menuActivate(event));
+projectsLink.addEventListener('click', (event) => menuActivate(event));
